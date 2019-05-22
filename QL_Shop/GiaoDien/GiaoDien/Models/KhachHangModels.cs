b08b5405853a496ddd1147a435a88f466b3a7713
NhuHuy <dangnhuhuy682@gmail.com>
2019-05-23 01:51:59 +0700

    commit

--- QL_Shop/GiaoDien/GiaoDien/Models/KhachHangModels.cs
+++ QL_Shop/GiaoDien/GiaoDien/Models/KhachHangModels.cs
@@ -29,9 +29,9 @@ namespace GiaoDien.Models
         {
             return _khachHangContronllass.GetEmployessCde(EmployessCde.ToString());
         }
-        public DataTable InsertEmployess( string ma, string ten, string diachi,string sdt)
+        public DataTable InsertEmployess( string ma, string mathe, string ten, string diachi,string sdt)
         {
-            return _khachHangContronllass.InsertEmployess(ma, ten, diachi, sdt);
+            return _khachHangContronllass.InsertEmployess(ma, mathe, ten, diachi, sdt);
         }
         public bool DeleteEmployess(string ma)
         {
