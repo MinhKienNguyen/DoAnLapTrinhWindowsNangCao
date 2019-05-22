b77d581e5bff40cc0524fffc49e2be235fcc2aab
NhuHuy <dangnhuhuy682@gmail.com>
2019-05-22 17:06:38 +0700

    commit HangHoaControllers

--- QL_Shop/GiaoDien/GiaoDien/Controllers/HangHoaControllers.cs
+++ QL_Shop/GiaoDien/GiaoDien/Controllers/HangHoaControllers.cs
@@ -20,20 +20,35 @@ namespace GiaoDien.Controllers
             var result = _unity.filldb("HH_HangHoa_GetImage", EmployessCde.ToString());
             return result;
         }
-        public DataTable InsertEmployess( string ma ,byte[] Image)
-        {
-            return _unity.filldb("HH_HangHoa_InsertEmployess",ma, Image);
-        }
 
+        public bool UpdateGiaHinhAn(string ma, string mau, byte[] Image, string giaban)
+        {
+            using (var tran = new System.Transactions.TransactionScope(System.Transactions.TransactionScopeOption.Required,
+            new System.Transactions.TransactionOptions { IsolationLevel = System.Transactions.IsolationLevel.ReadUncommitted }))
+            {
+                DataTable dtNV = _unity.filldb("HH_CT_HangHoa_UpDate", ma, Image, mau);
+                if (dtNV.Rows.Count <= 0 || Convert.ToInt16(dtNV.Rows[0][0].ToString()) <= 0)
+                {
+                    return false;
+                }
+                DataTable dtND = _unity.filldb("HH_HangHoa_Update", ma, giaban);
+                if (dtND.Rows.Count <= 0 || Convert.ToInt16(dtND.Rows[0][0].ToString()) <= 0)
+                {
+                    return false;
+                }
+                tran.Complete();
+                return true;
+            }
+        }
         public DataTable GetDataHangHoaDonHang(string _maNhapHang)
         {
             return _unity.filldb("PH_CT_DatHang_GetData", _maNhapHang);
         }
 
-        public DataTable InsertHangHoa(string _maHang, string _tenLoaiHang, string _barcode, string _tenHang, byte[] Image, string _maLoaiHang, string _maGia)
-        {
-            return _unity.filldb("HH_HangHoa_InsertOrUpdate", _maHang, _tenLoaiHang, _barcode, _tenHang, Image, _maLoaiHang, _maGia);
-        }
+        //public DataTable InsertHangHoa(string _maHang, string _tenLoaiHang, string _barcode, string _tenHang, byte[] Image, string _maLoaiHang, string _maGia)
+        //{
+        //    return _unity.filldb("HH_HangHoa_InsertOrUpdate", _maHang, _tenLoaiHang, _barcode, _tenHang, Image, _maLoaiHang, _maGia);
+        //}
 
         //public DataTable InsertCTHangHoa(string _maCT, string _maHang, string _maMau, string _maDVT, string _maSize, int _sl)
         //{
