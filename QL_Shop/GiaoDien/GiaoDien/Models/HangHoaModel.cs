fd140fd0e3a608520c9cc2b6c54b630e2d1c3d06
kiennm <kiennm@dsoft.software>
2019-05-22 17:00:43 +0700

    commit

--- QL_Shop/GiaoDien/GiaoDien/Models/HangHoaModel.cs
+++ QL_Shop/GiaoDien/GiaoDien/Models/HangHoaModel.cs
@@ -20,6 +20,12 @@ namespace GiaoDien.Models
             return _hangHoaControllers.GetImage(EmployessCde.ToString());
         }
 
+        /// <summary>
+        /// update hình ảnh
+        /// </summary>
+        /// <param name="ma"></param>
+        /// <param name="Image"></param>
+        /// <returns></returns>
         public DataTable InsertEmployess(string ma, byte[] Image)
         {
             return _hangHoaControllers.InsertEmployess(ma,Image);
@@ -32,6 +38,21 @@ namespace GiaoDien.Models
         public DataTable Getlkloaihang()
         {
             return _hangHoaControllers.Getlkloaihang();
+        }
+
+        public bool InsertHangHoa(string _maHang, string _tenHang, string _maLoaiHang, string _maCT, string _barcode, string _maMau, string _maDVT, string _maSize, int _SL)
+        {
+            return _hangHoaControllers.InsertHangHoa(_maHang, _tenHang, _maLoaiHang, _maCT, _barcode, _maMau, _maDVT, _maSize, _SL);
+        }
+
+        public bool UPdateStatusAndQuantity(string _maDH, int _trangThai, string _maCT_DH, int _SLGiao)
+        {
+            return _hangHoaControllers.UPdateStatusAndQuantity(_maDH, _trangThai, _maCT_DH, _SLGiao);
+        }
+
+        public DataTable GetDetailProduct(string _maCTHH)
+        {
+            return _hangHoaControllers.GetDetailProduct(_maCTHH);
         }
     }
 }
