a39561db20e6c9bf9577cc29e3116e9465dae2a5
kiennm <kiennm@dsoft.software>
2019-05-25 07:26:02 +0700

    commit

--- QL_Shop/GiaoDien/GiaoDien/Models/DoiTraModel.cs
+++ QL_Shop/GiaoDien/GiaoDien/Models/DoiTraModel.cs
@@ -16,5 +16,31 @@ namespace GiaoDien.Models
         {
             return _doiControllers.GetDataBill(_maHD);
         }
+
+        
+        public bool UpdateSL_CTHH(string _barcode, int _soLuong)
+        {
+            return _doiControllers.UpdateSL_CTHH(_barcode, _soLuong);
+        }
+
+        public bool Update_CTHD(string _barcode, int _soLuong, float _tienTra)
+        {
+            return _doiControllers.Update_CTHD(_barcode, _soLuong, _tienTra);
+        }
+
+        public bool Update_HD(string _maHD, float _tienTra)
+        {
+            return _doiControllers.Update_HD(_maHD, _tienTra);
+        }
+
+        public bool Insert_HDTra(string _maHDTra, string _maHD, string _maNV, float _tienTra)
+        {
+            return _doiControllers.Insert_HDTra(_maHDTra, _maHD, _maNV, _tienTra);
+        }
+
+        public bool Insert_CT_HDTra(string _maCTietHDTra, string _maHDTra, string _maHH, string _barcode, int _soLuong, float _thanhTien)
+        {
+            return _doiControllers.Insert_CT_HDTra(_maCTietHDTra, _maHDTra, _maHH, _barcode, _soLuong, _thanhTien);
+        }
     }
 }
