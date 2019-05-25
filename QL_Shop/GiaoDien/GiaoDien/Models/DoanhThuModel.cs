f95112950882bd43f69232d1bb874db9326bf6d4
NhuHuy <dangnhuhuy682@gmail.com>
2019-05-26 01:42:32 +0700

    commit

---
+++ QL_Shop/GiaoDien/GiaoDien/Models/DoanhThuModel.cs
@@ -0,0 +1,20 @@
+﻿using GiaoDien.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiaoDien.Models
+{
+   public class DoanhThuModel
+    {
+        DoanhThuContronllers _doanhThuContronllers = new DoanhThuContronllers();
+        public DataTable GetDoanhthu(DateTime _tuNgay, DateTime _denNgay)
+        {
+            return _doanhThuContronllers.GetDoanhthu(Convert.ToDateTime(_tuNgay),Convert.ToDateTime(_denNgay));
+
+        }
+    }
+}
