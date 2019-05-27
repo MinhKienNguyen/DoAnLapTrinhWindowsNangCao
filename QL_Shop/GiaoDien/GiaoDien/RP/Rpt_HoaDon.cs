0e1ffda59a1831356e966b558536ea4fd90600fc
kiennm <kiennm@dsoft.software>
2019-05-28 02:58:35 +0700

    commit

--- QL_Shop/GiaoDien/GiaoDien/RP/Rpt_HoaDon.cs
+++ QL_Shop/GiaoDien/GiaoDien/RP/Rpt_HoaDon.cs
@@ -18,7 +18,7 @@ namespace GiaoDien.RP
                 this.colKhachHang.DataBindings.Add("Text", dt, "TenKhachHang");
                 this.colNgay.DataBindings.Add("Text", dt, "NgayLapHD");
                 this.ColNhanVien.DataBindings.Add("Text", dt, "TenNhanVien");
-                this.colTongTien.DataBindings.Add("Text", dt, "TongTien","{0:N0} VNĐ");
+                this.colTongTien.DataBindings.Add("Text", dt, "TongTien", "{0:N0} VNĐ");
             }
         }
 
