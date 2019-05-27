f49736715bebe2bfc60782854450da419e1e7f46
kiennm <kiennm@dsoft.software>
2019-05-28 04:21:44 +0700

    commit

---
+++ QL_Shop/GiaoDien/GiaoDien/RP/RP_PrintReBill.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace GiaoDien.RP
+{
+    public partial class RP_PrintReBill : DevExpress.XtraEditors.XtraForm
+    {
+        DataTable iDataSource = null;
+        DataTable iDataSourceHeader = null;
+        public RP_PrintReBill( DataTable iData, DataTable DataHeader)
+        {
+            InitializeComponent();
+            iDataSource = iData;
+            iDataSourceHeader = DataHeader;
+        }
+
+        private void RP_HoaDon_Load(object sender, EventArgs e)
+        {
+            Rpt_PrintReBill rpt = new Rpt_PrintReBill();
+            rpt.DataSource = iDataSource;
+            rpt.BindHeader(iDataSourceHeader);
+            rpt.BindData();
+            documentViewer1.PrintingSystem = rpt.PrintingSystem;
+            rpt.CreateDocument();
+        }
+    }
+}
---
+++ QL_Shop/GiaoDien/GiaoDien/RP/Rpt_PrintReBill.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+
+namespace GiaoDien.RP
+{
+    public partial class Rpt_PrintReBill : Report
+    {
+        private int iNo = 1;
+        public Rpt_PrintReBill()
+        {
+            InitializeComponent();
+        }
+        public override void BindHeader(object _obj)
+        {
+            DataTable dt = (DataTable)_obj;
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                this.colMaHD.DataBindings.Add("Text", dt, "MaHoaDon");
+                this.colKhachHang.DataBindings.Add("Text", dt, "TenKhachHang");
+                this.colNgay.DataBindings.Add("Text", dt, "NgayLapHD");
+                this.ColNhanVien.DataBindings.Add("Text", dt, "TenNhanVien");
+                this.colTongTien.DataBindings.Add("Text", dt, "TongTien", "{0:N0} VNĐ");
+            }
+        }
+
+        public override void BindData()
+        {
+            this.colT1.DataBindings.Add("Text", this.DataSource, "TenHangHoa");
+            this.colT2.DataBindings.Add("Text", this.DataSource, "TenMau");
+            this.colT3.DataBindings.Add("Text", this.DataSource, "TenSize");
+            this.colT4.DataBindings.Add("Text", this.DataSource, "SoLuong", "{0:#,##0.##}");
+            this.colT5.DataBindings.Add("Text", this.DataSource, "TenDonViTinh");
+            this.colT6.DataBindings.Add("Text", this.DataSource, "GiaBan", "{0:N0} VNĐ");
+            this.colT7.DataBindings.Add("Text", this.DataSource, "ThanhTien", "{0:N0} VNĐ");
+        }
+        private void xrTable1_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            this.colNo.Text = iNo.ToString();
+            iNo += 1;
+        }
+    }
+}
